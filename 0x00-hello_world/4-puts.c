cf1baba9ad9a501ab1bdee26a5168a4a1ae5e236
tamratf <tamratfeleke@gmail.com>
2022-07-01 04:23:06 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * puts - Print a text using puts function
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
