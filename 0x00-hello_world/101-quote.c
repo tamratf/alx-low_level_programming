26418084cf47e1aa178c53b40bed10c2f85bbe1e
tamratf <tamratfeleke@gmail.com>
2022-07-01 05:14:58 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char txt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	char txt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fprintf(stderr, "%s\n", txt);
 	return (1);
