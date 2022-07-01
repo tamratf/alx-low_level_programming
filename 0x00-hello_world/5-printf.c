4d4c71bc22643311e2d8c5e8da92a7d87f81ab85
tamratf <tamratfeleke@gmail.com>
2022-07-01 04:26:31 +0300

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * puts - Print a text using printf function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
