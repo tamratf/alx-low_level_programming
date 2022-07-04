c9cf8f330d993e344996b98655e4b5e9009d5460
tamratf <tamratfeleke@gmail.com>
2022-07-04 18:23:01 +0300

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 5-print_numbers.c
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		printf(i);
+	}
+	putchar('\n');
+	return (0);
+}
