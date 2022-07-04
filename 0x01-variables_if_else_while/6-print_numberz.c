8058a9ac4fb9341359b0d470476d452fd712464b
tamratf <tamratfeleke@gmail.com>
2022-07-04 18:32:28 +0300

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 3-print_alphabets.c
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	for (; c <= 9; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
