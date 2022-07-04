1e4811a832a3aa2b377d38e9a375200dc81b3209
tamratf <tamratfeleke@gmail.com>
2022-07-04 18:10:51 +0300

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 3-print_alphabets.c
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	c = 'A'
+	for (; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
