abd561ab72a53392bf42ce0345a2b0899f033dc9
tamratf <tamratfeleke@gmail.com>
2022-07-05 01:18:46 +0300

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 101-print_comb4.c
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = '0';
+	int b = '0';
+	int c = '0';
+	int d = '1';
+	
+	for (; a <= '9'; a++){
+		for (; b <= '9'; b++)
+		{
+			for (; c <= '9'; c++)
+			{
+				for (; d <= '9'; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (a == '9' && b == '8' && c == '9' && c == '9')
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+				d = '0';
+			}
+			c = a;
+			d = b + 2;
+		}
+		b = '0';
+	}
+	putchar('\n');
+	return (0);
+}
+
