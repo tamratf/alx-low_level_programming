e878d7d80e781863957077787aaf78058a521d5d
tamratf <tamratfeleke@gmail.com>
2022-07-04 23:37:39 +0300

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 101-print_comb4.c
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int e = '0';
+	int i = '1';
+	int j = '2';
+
+	for (; e <= '7'; e++){
+		for (; i <= '8'; i++)
+		{
+			for (; j <= '9'; j++)
+			{
+				if (e == i || e == j || i == j);
+					continue;
+				putchar(e);
+				putchar(i);
+				putchar(j);
+				if (e == '7' && i == '8' && j == '9')
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+			j = i + 1;
+		}
+		i = e +1 ;
+	}
+	putchar('\n');
+	return (0);
+}
