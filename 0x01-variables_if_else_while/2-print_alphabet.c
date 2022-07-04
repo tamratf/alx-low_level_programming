c2542e3da5ec601f04aaadb5f9202f74b3a28cd8
tamratf <tamratfeleke@gmail.com>
2022-07-04 17:57:33 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 2-print_alphabet.c
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
