8a08350dbc7e242c365f09ab9c4e882c506d0d33
tamratf <tamratfeleke@gmail.com>
2022-07-04 22:50:48 +0300

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 9-print_comb.c
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char = '0';
+
+	for (; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(' ');
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
