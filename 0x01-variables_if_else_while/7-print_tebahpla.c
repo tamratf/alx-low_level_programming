de30f5d42d2c13c0aa5b99f855ba2ce6429c332d
tamratf <tamratfeleke@gmail.com>
2022-07-04 22:38:33 +0300

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 7-print_tebahpla.c
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'z';
+
+	for (; c <= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
