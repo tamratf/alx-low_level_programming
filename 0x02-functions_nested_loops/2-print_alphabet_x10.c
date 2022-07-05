aa1b82f71a9309bf5e874e5b5772ee08f718e9fb
tamratf <tamratfeleke@gmail.com>
2022-07-05 23:16:27 +0300

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * 2-print_alphabet_x10.c
+ * Return: Always 0 (Success)
+ */
+void  print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i = 0;
+
+	for (; i < <9; i++){
+		for (; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
