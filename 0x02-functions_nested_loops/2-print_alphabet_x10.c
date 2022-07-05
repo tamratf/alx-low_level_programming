6c5d32624e3948f65239cd99e5f3d30be02c52e0
tamratf <tamratfeleke@gmail.com>
2022-07-05 23:22:50 +0300

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,13 @@ void  print_alphabet_x10(void)
 	char c = 'a';
 	int i = 0;
 
-	for (; i < 9; i++){
+	for (; i < 9; i++)
+	{
 		for (; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
 		_putchar('\n');
+		c = 'a';
 	}
 }
