ccd19a7c0a9f3b43b1515a972231656d3138a4d8
tamratf <tamratfeleke@gmail.com>
2022-07-06 23:53:11 +0300

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,12 +12,12 @@ int print_last_digit(int n)
 	if (d < 0)
 	{
 		d = d * -1;
-		_putchar(d);
+		_putchar(d + '0');
 		return (d);
 	}
 	else
 	{
-		_putchar(d);
+		_putchar(d + '0');
 		return (d);
 	}
 }
