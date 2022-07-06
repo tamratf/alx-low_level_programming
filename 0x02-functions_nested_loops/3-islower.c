5dbd34c3c644806ac92942b34a45a09023e14069
tamratf <tamratfeleke@gmail.com>
2022-07-06 10:29:02 +0300

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * 3-islower.c
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c <= 'z')
+		return (1);
+	else
+		return (0)
+}
