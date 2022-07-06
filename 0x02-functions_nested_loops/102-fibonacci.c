3d4088e35c63ac22a566ee5a263fab80427c1d16
tamratf <tamratfeleke@gmail.com>
2022-07-07 01:05:53 +0300

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,11 +11,11 @@ int main(void)
 	int b = 1;
 	long int s;
 
-	printf("%d", a);
+	printf("%ld", a);
 	for (n = 1; n < 50; ++n)
 	{
 		s = a + b;
-		printf(", %d", s);
+		printf(", %ld", s);
 		a = b;
 		b = s;
 	}
