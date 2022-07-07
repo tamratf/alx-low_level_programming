f1fe607b3f865b7057cc48880eaf3263dcc9f351
tamratf <tamratfeleke@gmail.com>
2022-07-07 21:59:37 +0300

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,10 +3,6 @@
 
 #include <stdio.h>
 
-int _putchar(char c);
 void positive_or_negative(int i);
-int largest_number(int x, int y, int z);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
